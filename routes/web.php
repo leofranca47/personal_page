a777ed2b6e092fb47f8b1817d40cbc00c041f0ec
leofranca47 <leoprofissional@live.com>
2023-10-07 06:30:01 -0300

    testa husky

--- routes/web.php
+++ routes/web.php
@@ -23,11 +23,11 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::prefix('/profile')->group(function () {
+    Route::prefix('/profiles')->group(function () {
         Route::get('/', [ProfileController::class, 'edit'])->name('profile.edit');
         Route::patch('/', [ProfileController::class, 'update'])->name('profile.update');
         Route::delete('/', [ProfileController::class, 'destroy'])->name('profile.destroy');
     });
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
